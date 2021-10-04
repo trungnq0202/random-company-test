02adc9edaa36eb5db0f4669fa2d6d137a53f3b23
trungnq0202 <trungnqcs2000@gmail.com>
2021-10-04 21:29:10 +0700

    adjust 404 NotFound page

--- task2_and_3/frontend-react/src/pages/error/NotFound/NotFound.js
+++ task2_and_3/frontend-react/src/pages/error/NotFound/NotFound.js
@@ -1,22 +1,25 @@
 import { Container, Row, Col } from "react-bootstrap";
+import Layout from "../../../hoc/Layout/Layout";
 
-import './NotFound.css'
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <Container>
-      <Row>
-        <Col md={12} className="mx-auto mt-5">
-          <div class="error-template">
-            <h1>Oops!</h1>
-            <h2>404 Not Found</h2>
-            <div class="error-details">
-              Sorry, an error has occured, Requested page not found!
+    <Layout>
+      <Container>
+        <Row>
+          <Col md={12} className="mx-auto mt-5">
+            <div class="error-template">
+              <h1>Oops!</h1>
+              <h2>404 Not Found</h2>
+              <div class="error-details">
+                Sorry, an error has occured, Requested page not found!
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 };
 
