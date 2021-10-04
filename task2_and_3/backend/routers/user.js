b5b115759eaab47c43e3775640d189eaad5956b6
trungnq0202 <trungnqcs2000@gmail.com>
2021-10-04 21:22:38 +0700

    finish users APIs

--- task2_and_3/backend/routers/user.js
+++ task2_and_3/backend/routers/user.js
@@ -1,46 +1,76 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require('../middleware/authentication')
 const router = new express.Router();
 
-router.post("/api/users/add", async (req, res) => {
+router.post("/api/users/add", auth, async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
     res.status(201).send({ user });
   } catch (e) {
-    console.log(e);
     res.status(400).send(e);
   }
 });
 
-router.get("/api/users/get-all-by-name", async (req, res) => {
+router.get("/api/users/get-all-by-name", auth, async (req, res) => {
   try {
     const pattern = ".*" + req.query.name + ".*";
     const users = await User.find({
       $or: [
         {
-          'username': {
+          username: {
             $regex: pattern,
           },
         },
 
         {
-          'email': {
+          email: {
             $regex: pattern,
           },
         },
       ],
     });
-    
+
     res.send(users);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.post("/api/users/update-multiple", async (req, res) => {
-    
-})
+router.post("/api/users/update-multiple", auth, async (req, res) => {
+  const updatedUsers = req.body;
+  const finalizedUsers = [];
+  let containedErrorFlag = false;
+
+  try {
+    for (let i = 0; i < updatedUsers.length; ++i) {
+      let toBeUpdatedUser = { ...updatedUsers[i] };
+      const id = toBeUpdatedUser._id;
+      delete toBeUpdatedUser._id;
+
+      try {
+        const updatedUser = await User.findByIdAndUpdate(id, toBeUpdatedUser, {
+          new: true,
+          runValidators: true,
+        });
+        if (!updatedUser) continue;
+        finalizedUsers.push(updatedUser);
+      } catch (error) {
+        containedErrorFlag = true;
+      }
+    }
+    containedErrorFlag
+      ? res.send({
+          errorCode: 1,
+          finalizedUsers: finalizedUsers,
+        })
+      : res.send({ errorCode: null, finalizedUsers: finalizedUsers });
+  } catch (error) {
+    if (finalizedUsers.length === 0)
+      res.status(400).send({ errorCode: 2 });
+  }
+});
 
 module.exports = router;
