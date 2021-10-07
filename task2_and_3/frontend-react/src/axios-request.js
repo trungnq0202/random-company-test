a210d4f78725cead4674d7698dfe91b18016d3dd
trungnq0202 <trungnqcs2000@gmail.com>
2021-10-07 08:57:09 +0700

    fix wrong axios request base url

--- task2_and_3/frontend-react/src/axios-request.js
+++ task2_and_3/frontend-react/src/axios-request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://localhost:8080/api/'
-    baseURL: 'http://http://18.118.215.192/api/'
+    baseURL: 'http://18.118.215.192/api/'
 });
 
 export default instance;
