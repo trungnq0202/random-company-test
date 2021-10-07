8a6f502cfa094aa500b65caa204ec64f34a5a135
trungnq0202 <trungnqcs2000@gmail.com>
2021-10-07 08:43:51 +0700

    fix axios baseURL for deployment

--- task2_and_3/frontend-react/src/axios-request.js
+++ task2_and_3/frontend-react/src/axios-request.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/'
+    // baseURL: 'http://localhost:8080/api/'
+    baseURL: 'http://http://18.118.215.192/api/'
 });
 
 export default instance;
